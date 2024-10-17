d6d086284009b758e799d573fd4952f4a9940aaf
ViniciusVieiraJS <silvyo.vieira@sptech.school>
2024-10-17 00:13:42 -0300

    DTO de Ongs prontas | lista estatica para top 2 personalidades de cada animal de cada ong

---
+++ src/main/java/com/example/adpotme_api/dto/personalidade/PersonalidadeAnimalOngResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.adpotme_api.dto.personalidade;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonalidadeAnimalOngResponseDto {
+    private String personalidade;
+    private Integer valor;
+}
---
+++ src/main/java/com/example/adpotme_api/mapper/OngMapper.java
@@ -0,0 +1,34 @@
+package com.example.adpotme_api.mapper;
+
+import com.example.adpotme_api.dto.animal.AnimalOngResponseDto;
+import com.example.adpotme_api.dto.ong.OngResponseDto;
+import com.example.adpotme_api.entity.animal.Animal;
+import com.example.adpotme_api.entity.ong.Ong;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OngMapper {
+    public static OngResponseDto toOngResponseDto(Ong ong) {
+        OngResponseDto ongResponseDto = new OngResponseDto();
+        ongResponseDto.setId(ong.getId());
+        ongResponseDto.setNome(ong.getNome());
+        ongResponseDto.setEmail(ong.getEmail());
+        ongResponseDto.setTelefone(ong.getTelefone());
+        ongResponseDto.setCnpj(ong.getCnpj());
+        ongResponseDto.setEndereco(EnderecoMapper.toEnderecoResponseOngDto(ong.getEndereco()));
+        ongResponseDto.setAnimais(toAnimalOngResponseDto(ong.getAnimal()));
+        return ongResponseDto;
+
+    }
+
+    public static List<AnimalOngResponseDto> toAnimalOngResponseDto(List<Animal> animais) {
+        List<AnimalOngResponseDto> animaisResposta = new ArrayList<>();
+        for (Animal animal : animais) {
+            animaisResposta.add(AnimalMapper.toAnimalOngResponseDto(animal));
+        }
+
+        return animaisResposta;
+    }
+}
